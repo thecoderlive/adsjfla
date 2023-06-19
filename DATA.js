5ea8005491d77f05016b88f9857438c16b1d2e4d
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-06-19 12:17:55 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,30 @@
+export const item = {
+    "id": "0c18beef-ae26-46de-91c0-7e2e669351f9",
+    "search_results": [
+        {
+            "id": "6ea4bdbf-5931-4ea1-8152-fa12a040123b",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png",
+            "art_caption": "weight"
+        },
+        {
+            "id": "f7163d10-67ae-4418-8ac5-4e4048100f85",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png",
+            "art_caption": "walk"
+        },
+        {
+            "id": "9619ab99-6dc0-47ff-bc35-86d5a73a91ec",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png",
+            "art_caption": "theory"
+        },
+        {
+            "id": "473e4f62-a2d1-479d-8208-2de77f0436cd",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png",
+            "art_caption": "modern"
+        },
+        {
+            "id": "2e3cc932-52fc-4a08-9923-ffa974d5021f",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png",
+            "art_caption": "stand"
+        }
+    ]
+}
